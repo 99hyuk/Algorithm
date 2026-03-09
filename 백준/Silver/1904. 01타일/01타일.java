7c387f5d1ccbbb13088c9c08483ab0cbf2373404
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-03-09 13:00:38 +0900

    [Silver III] Title: 01타일, Time: 184 ms, Memory: 21632 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] dp = new int[n+1];
+		dp[1] = 1;
+		
+		for (int i=2; i<=n; i++) {
+			if (i==2) {
+				dp[2] = 2;
+				continue;
+			}
+			
+			dp[i] = (dp[i-2] + dp[i-1]) % 15746;
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
