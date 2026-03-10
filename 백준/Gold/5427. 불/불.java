73d9caf78ef4422f584fedb78e5db287e43e143d
조용혁 <95476328+99hyuk@users.noreply.github.com>
2026-03-10 09:31:28 +0900

    [Gold IV] Title: 불, Time: 812 ms, Memory: 83668 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -8,12 +8,10 @@ import java.util.StringTokenizer;
 class NodeFire {
 	int r;
 	int c;
-	int bool;
 	int time;
-	public NodeFire(int r, int c, int bool, int time) {
+	public NodeFire(int r, int c, int time) {
 		this.r = r;
 		this.c = c;
-		this.bool = bool;
 		this.time = time;
 	}
 }
@@ -22,10 +20,13 @@ public class Main {
 	static int[] dr = {1,-1,0,0};
 	static int[] dc = {0,0,-1,1};
 	static int w, h;
+	static char[][] map;
+	static int[][] fireMap;
+	static Queue<NodeFire> QF;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int T = Integer.parseInt(br.readLine());
@@ -35,17 +36,25 @@ public class Main {
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
 			
-			char[][] map = new char[h][w];
-			Queue<NodeFire> Q = new ArrayDeque<>();
+			map = new char[h][w];
+			fireMap = new int[h][w];
+			QF = new ArrayDeque<>();
 			int sangaenR = -1;
 			int sangaenC = -1;
+			
+			for (int i=0; i<h; i++) {
+				for (int j=0; j<w; j++) {
+					fireMap[i][j] = -1;
+				}
+			}
 			
 			for (int i=0; i<h; i++) {
 				String str = br.readLine();
 				for (int j=0; j<w; j++) {
 					map[i][j] = str.charAt(j);
 					if (map[i][j] == '*') {
-						Q.offer(new NodeFire(i, j, 1, 0));
+						QF.offer(new NodeFire(i, j, 0));
+						fireMap[i][j] = 0;
 					}
 					if (map[i][j] == '@') {
 						sangaenR = i;
@@ -54,55 +63,58 @@ public class Main {
 				}
 			}
 			
-			boolean finish = false;
-			if (sangaenR==0 || sangaenR==h-1 || sangaenC==0 || sangaenC==w-1) {
-				finish = true;
-				sb.append(1+"\n");
-			}
-			
-			Q.offer(new NodeFire(sangaenR, sangaenC, 0, 0));
-			
-			while(!Q.isEmpty() && !finish) {
-				NodeFire node = Q.poll();
-				
-				if (node.bool == 1) {
-					for (int ro=0; ro<4; ro++) {
-						int nr = node.r + dr[ro];
-						int nc = node.c + dc[ro];
-						
-						if(!isIn(nr,nc)) continue;
-						if(map[nr][nc] == '#' || map[nr][nc] == '*') continue;
-						
-						map[nr][nc] = '*';
-						Q.offer(new NodeFire(nr, nc, node.bool, node.time+1));
-					}
-				} else {
-					for (int ro=0; ro<4; ro++) {
-						int nr = node.r + dr[ro];
-						int nc = node.c + dc[ro];
-						
-						if(!isIn(nr,nc)) continue;
-						if(map[nr][nc] == '#' || map[nr][nc] == '*' || map[nr][nc] == '@') continue;
-						
-						map[nr][nc] = '@';
-						Q.offer(new NodeFire(nr, nc, node.bool, node.time+1));
-						
-						if(nr==0 || nr==h-1 || nc==0 || nc==w-1) {
-							sb.append(node.time+2 + "\n");
-							finish = true;
-							break;
-						}
-					}
+			bfsFire();
+			bfsSangaen(sangaenR, sangaenC);
 					
-				}
-			}
-			
-			if(!finish) {
-				sb.append("IMPOSSIBLE\n");
-			}
 		}
 		
 		System.out.println(sb);
+	}
+	
+	static void bfsFire() {
+		
+		while(!QF.isEmpty()) {
+			NodeFire node = QF.poll();
+			
+			for (int ro=0; ro<4; ro++) {
+				int nr = node.r + dr[ro];
+				int nc = node.c + dc[ro];
+				
+				if(!isIn(nr,nc)) continue;
+				if(map[nr][nc] == '#' || map[nr][nc] == '*') continue;
+				
+				map[nr][nc] = '*';
+				fireMap[nr][nc] = node.time+1;
+				QF.offer(new NodeFire(nr, nc, node.time+1));
+			}
+		}
+	}
+	
+	static void bfsSangaen(int r, int c) {
+		Queue<NodeFire> Q = new ArrayDeque<>();
+		Q.offer(new NodeFire(r, c, 0));
+		
+		while(!Q.isEmpty()) {
+			NodeFire node = Q.poll();
+			
+			for (int ro=0; ro<4; ro++) {
+				int nr = node.r + dr[ro];
+				int nc = node.c + dc[ro];
+				
+				if(!isIn(nr,nc)) {
+					sb.append(node.time+1 + "\n");
+					return;
+				}
+				if(map[nr][nc] == '#' || map[nr][nc] == '@') continue;
+				if(fireMap[nr][nc] != -1 && fireMap[nr][nc] <= node.time+1) continue;
+				
+				map[nr][nc] = '@';
+				Q.offer(new NodeFire(nr, nc, node.time+1));
+			}
+		}
+		
+		sb.append("IMPOSSIBLE\n");
+		return;
 	}
 	
 	static boolean isIn(int r, int c) {
